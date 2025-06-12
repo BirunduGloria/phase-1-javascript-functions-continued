5e196d248de785ac691dc32253917d4aa84a8732
Gloria Birundu <birundugloria@gmail.com>
2025-06-12 08:05:09 +0300

    function declaration

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun("go to church"));     
+console.log(saturdayFun());
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork()); 
+console.log(mondayWork("work from home")); 
+
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+const wrap = wrapAdjective("!");
+console.log(wrap("amazing")); 
+
+console.log(wrapAdjective("%")("a dedicated programmer")); 
